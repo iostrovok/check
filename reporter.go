3775d18262810571661e3b505c11c4ddfe763b56
Vladimir Lashko <vladimir@clutch.co>
2019-10-18 14:11:31 -0400

    Check output

--- reporter.go
+++ reporter.go
@@ -83,6 +83,7 @@ func (ow *outputWriter) WriteCallSuccess(label string, c *C) {
 
 func renderCallHeader(label string, c *C, prefix, suffix string) string {
 	pc := c.method.PC()
+	label = styleLabel(label)
 	return fmt.Sprintf("%s%s %s: %s%s", prefix, label, niceFuncPath(pc),
 		niceFuncName(pc), suffix)
 }
